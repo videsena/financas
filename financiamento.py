c9e37d4beb419744cd1e2c87b0b880b9cf8e8542
videsena <videsena@gmail.com>
2024-08-19 20:40:10 -0300

    adicionando extraordinarias

--- financiamento.py
+++ financiamento.py
@@ -1,32 +1,40 @@
-import calculators
+import calculos
 
-def tabela_sac(periodos: int, taxa: float, principal: float) -> list:
+def tabela_sac(periodos: int, taxa: float, principal: float, extraordinarias: dict = {}) -> list:
     tabela = []
-    tabela.append({'periodo': 0, 'saldo_devedor': principal, 'amortizacao': 0.0, 'juros': 0.0, 'parcela': 0.0})
+    tabela.append({'periodo': 0, 'saldo_devedor': principal, 'juros': 0.0, 'amortizacao_base': 0.0, 'amortizacao_extra': 0.0, 'parcela': 0.0})
 
     saldo_devedor = principal
-    for x in range(1,periodos+1):
-        amort = calculators.percentual(periodos, x) * saldo_devedor
-        juros = saldo_devedor * taxa
-        pgto = amort + juros
-        saldo_devedor = saldo_devedor - amort
+    for x in range(1, periodos + 1):
+        if saldo_devedor <= 0.0:
+            continue
 
-        tabela.append({'periodo': x, 'saldo_devedor': saldo_devedor, 'amortizacao': amort, 'juros': juros, 'parcela': pgto})
+        amort_base = calculos.percentual(periodos, x) * saldo_devedor
+        amort_extra = extraordinarias.get(x, 0.0)
+        juros = saldo_devedor * taxa
+        pgto_base = amort_base + juros
+        saldo_devedor = saldo_devedor - amort_base - amort_extra
+
+        tabela.append({'periodo': x, 'saldo_devedor': saldo_devedor, 'juros': juros, 'amortizacao_base': amort_base, 'amortizacao_extra': amort_extra,  'parcela': pgto_base + amort_extra})
 
     return tabela
 
-def tabela_price(periodos: int, taxa: float, principal: float) -> list:
+def tabela_price(periodos: int, taxa: float, principal: float, extraordinarias: dict = {}) -> list:
     tabela = []
-    tabela.append({'periodo': 0, 'saldo_devedor': principal, 'amortizacao': 0.0, 'juros': 0.0, 'parcela': 0.0})
+    tabela.append({'periodo': 0, 'saldo_devedor': principal, 'juros': 0.0, 'amortizacao_base': 0.0, 'amortizacao_extra': 0.0, 'parcela': 0.0})
 
     pgto = (principal * taxa) / (1 - (1 + taxa) ** (-periodos))
 
     saldo_devedor = principal
-    for x in range(1,periodos+1):
-        juros = saldo_devedor * taxa
-        amort = pgto - juros
-        saldo_devedor = saldo_devedor - amort
+    for x in range(1, periodos + 1):
+        if saldo_devedor <= 0.0:
+            continue
 
-        tabela.append({'periodo': x, 'saldo_devedor':saldo_devedor, 'amortizacao': amort, 'juros': juros, 'parcela': pgto})
+        juros = saldo_devedor * taxa
+        amort_base = pgto - juros
+        amort_extra = extraordinarias.get(x, 0.0)
+        saldo_devedor = saldo_devedor - amort_base - amort_extra
+
+        tabela.append({'periodo': x, 'saldo_devedor': saldo_devedor, 'juros': juros, 'amortizacao_base': amort_base, 'amortizacao_extra': amort_extra,  'parcela': pgto + amort_extra})
 
     return tabela
