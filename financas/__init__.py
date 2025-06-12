bc3ddd2079af6d25a402d707da9e33071b22dc41
u-vnogueira <vinicius.nogueira@nubank.com.br>
2025-06-12 11:24:37 -0300

    add impostos

--- financas/__init__.py
+++ financas/__init__.py
@@ -3,3 +3,4 @@ from . import financiamento
 from . import mat_fin
 from . import tools
 from . import analise_dados
+from . import impostos
---
+++ financas/impostos.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os, sys, pathlib
+
+def get_tabela_inss(ano):
+    tabela = pd.read_csv(f"{str(pathlib.Path().resolve())}/tabela_inss.csv").to_dict('records')
+    tabela = [t for t in tabela if t['ano'] == ano]
+    return tabela
+
+def get_tabela_irrf(ano):
+    tabela = pd.read_csv(f"{str(pathlib.Path().resolve())}/tabela_irrf.csv").to_dict('records')
+    tabela = [t for t in tabela if t['ano'] == ano]
+    return tabela
