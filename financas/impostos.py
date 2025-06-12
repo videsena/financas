4d18b6dd68eaf758b47c5819721fd5e258b99fab
u-vnogueira <vinicius.nogueira@nubank.com.br>
2025-06-12 11:26:21 -0300

    update

--- financas/impostos.py
+++ financas/impostos.py
@@ -2,11 +2,11 @@ import pandas as pd
 import os, sys, pathlib
 
 def get_tabela_inss(ano):
-    tabela = pd.read_csv(f"{str(pathlib.Path().resolve())}/tabela_inss.csv").to_dict('records')
+    tabela = pd.read_csv(f"{str(pathlib.Path().resolve())}/financas/tabela_inss.csv").to_dict('records')
     tabela = [t for t in tabela if t['ano'] == ano]
     return tabela
 
 def get_tabela_irrf(ano):
-    tabela = pd.read_csv(f"{str(pathlib.Path().resolve())}/tabela_irrf.csv").to_dict('records')
+    tabela = pd.read_csv(f"{str(pathlib.Path().resolve())}/financas/tabela_irrf.csv").to_dict('records')
     tabela = [t for t in tabela if t['ano'] == ano]
     return tabela
